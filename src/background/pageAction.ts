4730f140ff1b27cddac4b6efaa92c8f1707377e1
nekoziroo <nekoziroo@outlook.com>
2017-12-10 21:33:38 +0900

    Move Tab class to PageAction class

--- src/background/pageAction.ts
+++ src/background/pageAction.ts
@@ -1,19 +1,38 @@
+import config from '../config/config';
 import Download from './Download';
 import Message from './Message';
-import Tab from './Tab';
 import Thumbinfo from './Thumbinfo';
 
-export default class PageAction extends Tab {
+interface ActiveInfo {
+  tabId: number;
+}
+
+export default class PageAction {
   constructor() {
-    super(PageAction.show);
-    browser.pageAction.onClicked.addListener(PageAction.onClicked);
+    browser.tabs.onUpdated.addListener(this.onUpdated.bind(this));
+    browser.tabs.onActivated.addListener(this.onActivated.bind(this));
+    browser.pageAction.onClicked.addListener(this.onClicked.bind(this));
   }
 
-  private static show(tabId: number): void {
+  private onUpdated(tabId: number, changeInfo: Tabs.Tab): void {
+    if (changeInfo.status === 'complete') {
+      this.updateActiveTab(tabId);
+    }
+  }
+
+  private onActivated(activeInfo: ActiveInfo): void {
+    this.updateActiveTab(activeInfo.tabId);
+  }
+
+  private async updateActiveTab(tabId: number): Promise<void> {
+    const tabInfo = await browser.tabs.get(tabId);
+    if (!config.URL_NICONICO_WATCH.test(tabInfo.url)) {
+      return;
+    }
     browser.pageAction.show(tabId);
   }
 
-  private static async onClicked(tab: Tabs.Tab): Promise<void> {
+  private async onClicked(tab: Tabs.Tab): Promise<void> {
     const message = new Message(tab.id);
     const response = await message.get();
     const thumbinfo = new Thumbinfo(tab.url);
